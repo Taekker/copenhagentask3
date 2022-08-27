cf87db0fb4e389b8abd9a40843e9c753b0a3b93e
”Marie <mari576p@stud.kea.dk>
2022-08-27 10:17:21 +0200

    nyeste

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     colors: {
       red: "#B8001D",
       white: "#FFFFFF",
+      black: "#000000",
     },
     extend: {
       fontFamily: {
